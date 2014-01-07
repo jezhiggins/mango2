33453f64736cba6c7aefcfe945a252777abf20a8
Jez Higgins <jez@jezuk.co.uk>
2014-01-07 10:24:29 +0000

    Moved dropUntil and dropWhile tests into Drop

--- src/test/groovy/jezuk/Drop.groovy
+++ src/test/groovy/jezuk/Drop.groovy
@@ -1,6 +1,7 @@
 package jezuk
 
 import jezuk.mango.Mango
+import jezuk.mango.Predicate
 
 class Drop extends spock.lang.Specification {
   def "drop tests"() {
@@ -15,5 +16,39 @@ class Drop extends spock.lang.Specification {
       []                    |   5   | []
       [1,2,3,4,5]           |   0   | [1,2,3,4,5]
       [1,2,3,4,5]           |   1   | [2,3,4,5]
+  }
+
+  def "dropWhile tests"() {
+    def notFive = new Predicate<Integer>() {
+       public boolean test(Integer i) { return i != 5; }
+    }
+
+    expect:
+      Mango.from(list).dropWhile(notFive).toList() == result
+    where:
+      list                  | result
+      []                    | []
+      [1,2,3,4,5]           | [5]
+      [1,2,3,4,5,6,7,8]     | [5,6,7,8]
+      [5,5,5,5,5,5]         | [5,5,5,5,5,5]
+      [9,9,9,9,9]           | []
+      [9,9,9,9,9,5,8,8,8,8] | [5,8,8,8,8]
+  }
+
+  def "dropUntil tests"() {
+    def isFive = new Predicate<Integer>() {
+      public boolean test(Integer i) { return i == 5; }
+    }
+
+    expect:
+      Mango.from(list).dropUntil(isFive).toList() == result
+    where:
+      list                  | result
+      []                    | []
+      [1,2,3,4,5]           | [5]
+      [1,2,3,4,5,6,7,8]     | [5,6,7,8]
+      [5,5,5,5,5,5]         | [5,5,5,5,5,5]
+      [9,9,9,9,9]           | []
+      [9,9,9,9,9,5,8,8,8,8] | [5,8,8,8,8]
   }
 }
