5fa727bb7db88e3264175d7b9035ec14fe2162b5
Jez Higgins <jez@jezuk.co.uk>
2014-01-07 10:06:19 +0000

    Simplified drop tests to use a table

--- src/test/groovy/jezuk/Drop.groovy
+++ src/test/groovy/jezuk/Drop.groovy
@@ -3,44 +3,17 @@ package jezuk
 import jezuk.mango.Mango
 
 class Drop extends spock.lang.Specification {
-  def "drop 5 from 10"() {
-    when:
-      def t = dropFrom(5, [1,2,3,4,5,6,7,8,9,0])
-    then:
-      t == [6,7,8,9,0]
-  }
-  def "drop 5 from 3"() {
-    when:
-      def t = dropFrom(5, [1,2,3])
-    then:
-       t == []
-  }
-  def "drop 5 from 5"() {
-    when:
-      def t = dropFrom(5, [1,2,3,4,5])
-    then:
-       t == []
-  }
-  def "drop 5 from 0"() {
-    when:
-      def t = dropFrom(5, [])
-    then:
-       t == []
-  }
-  def "drop 0 from 5"() {
-    when:
-      def t = dropFrom(0, [1,2,3,4,5])
-    then:
-      t == [1,2,3,4,5]
-  }
-  def "drop 1 from 5"() {
-    when:
-      def t = dropFrom(1, [1,2,3,4,5])
-    then:
-      t == [2,3,4,5]
-  }
+  def "drop tests"() {
+    expect:
+      Mango.from(list).drop(count).toList() == result
 
-  def dropFrom(def count, def list) {
-    return Mango.from(list).drop(count).toList()
+    where:
+      list                  | count | result
+      [1,2,3,4,5,6,7,8,9,0] |   5   | [6,7,8,9,0]
+      [1,2,3]               |   5   | []
+      [1,2,3,4,5]           |   5   | []
+      []                    |   5   | []
+      [1,2,3,4,5]           |   0   | [1,2,3,4,5]
+      [1,2,3,4,5]           |   1   | [2,3,4,5]
   }
 }
