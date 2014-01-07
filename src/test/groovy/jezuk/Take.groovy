58cea8eda8b98443f8a85da8dfb175df975378aa
Jez Higgins <jez@jezuk.co.uk>
2014-01-07 09:55:47 +0000

    Startiong to get the hang of Spock. Rewrote test to use table for much clarification win

--- src/test/groovy/jezuk/Take.groovy
+++ src/test/groovy/jezuk/Take.groovy
@@ -3,49 +3,17 @@ package jezuk
 import jezuk.mango.Mango
 
 class Take extends spock.lang.Specification {
-  def "take 5 from 10"() {
-    when:
-      def l = [1,2,3,4,5,6,7,8,9,0];
-      def t = Mango.from(l).take(5).toList()
+  def "take tests" () {
+    expect:
+      Mango.from(s).take(n).toList() == result
 
-    then:
-       t == [1,2,3,4,5]
+    where:
+      s                     | n  | result
+      [1,2,3,4,5,6,7,8,9,0] | 5  | [1,2,3,4,5]
+      [1,2,3]               | 5  | [1,2,3]
+      [1,2,3,4,5]           | 5  | [1,2,3,4,5]
+      []                    | 5  | []
+      [1,2,3,4,5]           | 0  | []
+      [1,2,3,4,5]           | 1  | [1]
   }
-  def "take 5 from 3"() {
-    when:
-      def l = [1,2,3];
-      def t = Mango.from(l).take(5).toList()
-
-    then:
-       t == [1,2,3]
-  }
-  def "take 5 from 5"() {
-    when:
-      def l = [1,2,3,4,5];
-      def t = Mango.from(l).take(5).toList()
-
-    then:
-       t == [1,2,3,4,5]
-  }
-  def "take 5 from 0"() {
-    when:
-      def t = Mango.from([]).take(5).toList()
-    then:
-       t == []
-  }
-  def "take 0 from 5"() {
-    when:
-      def t = Mango.from([1,2,3,4,5]).take(0).toList()
-    then:
-      t == []
-  }
-  def "take 1 from 5"() {
-    when:
-      def t = Mango.from([1,2,3,4,5]).take(1)
-    then:
-      t.hasNext() == true
-      t.next() == 1
-      t.hasNext() == false
-  }
-
 }
