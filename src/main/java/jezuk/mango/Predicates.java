83f0425f27a923b3b7db9ef7d1e991a5d4b55f95
Jez Higgins <jez@jezuk.co.uk>
2014-07-11 16:13:57 +0100

    Added some Predicates

--- src/main/java/jezuk/mango/Predicates.java
+++ src/main/java/jezuk/mango/Predicates.java
@@ -3,6 +3,10 @@ package jezuk.mango;
 public final class Predicates {
   public static <T> Predicate<T> Not(Predicate<T> pred) { return new Not<T>(pred); }
   public static <T> Predicate<T> Counter(int count) { return new Counter<T>(count); }
+  public static <T> Predicate<T> Equal(T value) { return value != null ? new Equal<T>(value) : new IsNull<T>(); }
+  public static <T> Predicate<T> NotEqual(T value) { return value != null ? new NotEqual<T>(value) : new IsNotNull<T>(); }
+  public static <T> Predicate<T> Null() { return new IsNull<T>(); }
+  public static <T> Predicate<T> NotNull() { return new IsNotNull<T>(); }
 
   private static class Not<T> implements Predicate<T> {
     private final Predicate<T> pred_;
@@ -16,4 +20,24 @@ public final class Predicates {
     private Counter(int max) { max_ = max; count_ = 0; }
     public boolean test(T t) { return max_ > count_++; }
   } // class Counter
+
+  private static class Equal<T> implements Predicate<T> {
+    private final T value_;
+    private Equal(T value) { value_ = value; }
+    public boolean test(T t) { return value_.equals(t); }
+  } // class Equal
+
+  private static class NotEqual<T> implements Predicate<T> {
+    private final T value_;
+    private NotEqual(T value) { value_ = value; }
+    public boolean test(T t) { return !value_.equals(t); }
+  } // class NotEqual
+
+  private static class IsNull<T> implements Predicate<T> {
+    public boolean test(T t) { return t == null; }
+  } // class IsNull
+
+  private static class IsNotNull<T> implements Predicate<T> {
+    public boolean test(T t) { return t != null; }
+  } // class IsNotNull
 } // class Predicates
--- src/test/groovy/jezuk/Predicate.groovy
+++ src/test/groovy/jezuk/Predicate.groovy
@@ -1,6 +1,8 @@
 package jezuk
 
+import jezuk.mango.Mango
 import jezuk.mango.Predicate
+import jezuk.mango.Predicates
 
 class PredicateTest extends spock.lang.Specification {
   def "create and exercise a 'even integer' predicate"() {
@@ -14,5 +16,20 @@ class PredicateTest extends spock.lang.Specification {
     where:
       x << [0, 1, 2, 3, 4]
       truth << [true, false, true, false, true]
+  }
+
+  def "equal test"() {
+    expect:
+      Mango.from(list).where(Predicates.Equal(filter)).toList() == result
+
+    where:
+      list                    | filter  | result
+      [1, 2, 3]               | 1       | [ 1 ]
+      ['a', 'b', 'c']         | 'c'     | [ 'c' ]
+      ["one", "two", "three"] | "two"   | [ "two" ]
+      [1, 2, 3]               | 5       | [ ]
+      ['c', 'a', 'b', 'c']    | 'c'     | [ 'c', 'c' ]
+      ['c', null, 'b', 'c']   | null    | [ null ]
+      [1, 2, 3]               | null    | [ ]
   }
 }
